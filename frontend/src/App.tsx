5aa09e96286faa1c9eac5a367c23f62be5422e49
Bruno Veloso <brvelosoo@gmail.com>
2022-01-13 00:34:39 -0300

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import Navbar from './components/Navbar';
 
 
 function App() {
   return (
-   <h1>Página principal</h1>
+   <Navbar />
   );
 }
 
---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,21 @@
+import { ReactComponent as GitHubIcon } from 'assets/img/gitHub.svg';
+import './styles.css'
+function Navbar() {
+    return (
+        <header>
+            <nav className='container'>
+                <div className='dsmovie-nav-content'>
+                    <h1>DSMovie</h1>
+                    <a href="https://github.com/brunovelosodossantos26">
+                        <div className='dsmovie-contact-container'>
+                            <GitHubIcon />
+                            <p className='dsmovie-contact-link'>/devsuperior</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar;
